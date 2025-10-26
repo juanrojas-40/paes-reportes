a3c918f7b7e588f8e9025c95ad72e14615214724
JUAN ROJAS <juan.rojas.valencia@gmail.com>
2025-10-26 17:24:44 -0300

    feat: nuevo

--- appPUNTAJECIMMA.py
+++ appPUNTAJECIMMA.py
@@ -419,7 +419,7 @@ def process_file(uploaded_file, date_uploaded):
 # APP PRINCIPAL
 # ==============================
 st.set_page_config(page_title="Sistema PAES - Administrador", layout="wide", page_icon="logo.gif")
-st.image("logo.gif", width=200)
+st.image("logo.png", width=200)
 
 # Verificar secrets
 if not verificar_secrets():
